d9982adf2604a6ef5bd66a967344e39eb51a6a02
JoseHeitorPontes <joseheitorppdev@gmail.com>
2025-11-11 22:21:13 -0300

    feat: create main layout file

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Shalom Books Bi</title>
+
+        @vite([
+            "resources/css/app.css"
+        ])
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg bg-black mb-4">
+            <div class="container-fluid">
+                <h4 class="text-light">Shalom Books Bi</h4>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="/">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="/livros">Livros</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            @yield('body')
+        </div>
+
+        @vite([
+            "resources/js/app.js"
+        ])
+    </body>
+</html>
